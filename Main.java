4ededdb947ae5f8b5c44285495c4773204e0f444
pillanosl <pillanosl@eafit.edu.co>
2024-04-12 04:27:29 -0500

    Tareita ok

--- Main.java
+++ Main.java
@@ -6,14 +6,15 @@ public class Main {
     public static void main(String[] args) {
         crearMascota();
         consultarMascotas();
+        printName();
     }
 
 
     static void crearMascota() {
         Mascota m1 = new Mascota("Tobias", "Snouzer", "Canino", 5);
-        mascotas.add(m1);//agregamos la cascota a las lista de mascotas
+        mascotas.add(m1);//agregamos la mascota a las lista de mascotas
         Mascota m2 = new Mascota("Lilu", "Pequinez", "Canino", 15);
-        mascotas.add(m2);//agregamos la cascota a las lista de mascotas
+        mascotas.add(m2);//agregamos la mascota a las lista de mascotas
     }
 
     static void consultarMascotas() {
@@ -21,6 +22,10 @@ public class Main {
         for (Mascota m : mascotas) {
             System.out.println(m.getNombre());
         }
+    }
+
+    static void printName(){
+        System.out.println("Modificado por: Paula");
     }
 
 
