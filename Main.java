a17489dcf1885340d905d5a45e8b0287a5b62e4d
lhiguitab <lhiguitab@eafit.edu.co>
2024-04-12 11:13:24 -0500

    Imprime mi nombre :)

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         crearMascota();
         consultarMascotas();
+        imprimirNombre();
     }
 
     static void imprimirNombre(){
