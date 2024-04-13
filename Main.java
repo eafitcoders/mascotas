ac39ef4933a092c2e2d7b41d48737deb794bb623
JJRodriguezz <jjrodrigur@eafit.edu.co>
2024-04-12 23:37:50 -0500

    Se ha agregado la función JJRR para nombre del estudiante

--- Main.java
+++ Main.java
@@ -6,15 +6,15 @@ public class Main {
     public static void main(String[] args) {
         crearMascota();
         consultarMascotas();
-        nombre();
+        JJRR();
     }
 
 
     static void crearMascota() {
         Mascota m1 = new Mascota("Tobias", "Snouzer", "Canino", 5);
-        mascotas.add(m1);//agregamos la cascota a las lista de mascotas
+        mascotas.add(m1);//agregamos la mascota a las lista de mascotas
         Mascota m2 = new Mascota("Lilu", "Pequinez", "Canino", 15);
-        mascotas.add(m2);//agregamos la cascota a las lista de mascotas
+        mascotas.add(m2);//agregamos la mascota a las lista de mascotas
     }
 
     static void consultarMascotas() {
@@ -25,10 +25,20 @@ public class Main {
     }
 
     //Metodo para imprimir mi nombre
-    static void nombre(){
-        System.out.println("Nombre: Sofía Isaareth Flores Suárez");
-        System.out.println("Número de documento: 1127626894");
-        System.out.println("Grupo: Viernes 9-12m");
+    // static void nombre(){
+    //     System.out.println("Nombre: Sofía Isaareth Flores Suárez");
+    //     System.out.println("Número de documento: 1127626894");
+    //     System.out.println("Grupo: Viernes 9-12m");
+    // }
+
+    //Método para imprimir mi nombre (Y mascota en este caso)
+    static void JJRR(){
+        System.out.println("=====================================");
+        Mascota Toby = new Mascota("Toby", "Labrador,", "Canino", 9);
+        System.out.println("Nombre: Juan José Rodríguez Restrepo");
+        System.out.println("C.C: 1036449192");
+        System.out.println("Mascota: " + Toby.getNombre());
+        System.out.println("=====================================");
     }
 
 
