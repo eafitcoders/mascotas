7458f6cd46656f689ff9812f93909324451f0f24
Juan092805 <157924529+Juan092805@users.noreply.github.com>
2024-04-17 23:30:57 -0500

    agregar metodo nombre

--- Main.java
+++ Main.java
@@ -30,11 +30,4 @@ public class Main {
         System.out.println("Direccion de residencia: Cra 24 #43A-87");
         System.out.println("Grupo: Jueves 9am a 12am");
     }
-
-
-
-    static void eliminarMascotas() {
     }
-
-
-}
