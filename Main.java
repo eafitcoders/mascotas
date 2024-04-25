5784c0db96c7f7c302576fb5c2b3163f69533514
SamuelHenao404 <141289013+SamuelHenao404@users.noreply.github.com>
2024-04-25 11:14:13 -0500

    Tarea Samuel Henao Castrillon

--- Main.java
+++ Main.java
@@ -126,6 +126,10 @@ public class Main {
         System.out.println("Grupo: Jueves 9am-12pm");
     }
 
+ static void SamuelHenao(){
+        System.out.println("Nombre: Samuel Henao Castrillon");
+        System.out.println("Grupo: Jueves 9am-12pm");
+    }
 
 
     static void thomasBuitrago() {
